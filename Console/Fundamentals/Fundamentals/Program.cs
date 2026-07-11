2cb6920251b64b6caf65aed1c5521f41b34f0ae6
meproductive <me.productive0@gmail.com>
2026-07-12 00:44:26 +0300

    Add project structure - Fundamentals project description

---
+++ Console/Fundamentals/Fundamentals/Program.cs
@@ -0,0 +1,5 @@
+﻿/*
+    C# Programming Fundamentals
+    ------------------------------------------------------------------------------------------------------
+    This project contains the fundamental concepts of C# Programming, cobining theoretical explanations with practical examples.
+ */
